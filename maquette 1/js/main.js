654a5621481810ed646c71b7072160c58e002215
NabilBoussalem <nabilboussalem00@gmail.com>
2021-03-20 16:51:11 +0100

    Responsive navbar

---
+++ maquette 1/js/main.js
@@ -0,0 +1,16 @@
+var burger = document.querySelector(".burger");
+var header = document.querySelector(".header");
+
+var click=0;
+
+burger.onclick = function()
+        {
+            if (click == 0) {
+                header.style.height="calc((1.8em + 10px *2) * 5)";
+                click++;
+            } else {
+                 click=0;
+                header.style.height="33px";
+            }
+            
+        }
